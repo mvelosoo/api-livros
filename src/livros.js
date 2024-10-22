db1a6b0989dbcead4afb30a69e923cc10b0e5597
Maria-Eduarda-Sauter <mveloso@sauter.digital>
2024-10-21 22:54:46 -0300

    first

--- src/livros.js
+++ src/livros.js
@@ -3,18 +3,31 @@ const router = express.Router();
 
 let livros = [
     { id: 1, titulo: 'Silêncio dos Inocentes' },
-    { id: 2, titulo: 'O Mal-estar da civilização' }
+    { id: 2, titulo: 'O Mal-estar da civilização' },
 ];
 
+/**
+ * @swagger
+ * /livros:
+ *   get:
+ *     summary: Retorna a lista de livros
+ *     responses:
+ *       200:
+ *         description: Lista de livros retornada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   titulo:
+ *                     type: string
+ */
 router.get('/', (req, res) => {
     res.json(livros);
-});
-
-router.post('/', (req, res) => {
-    const { titulo } = req.body;
-    const novoLivro = { id: livros.length + 1, titulo };
-    livros.push(novoLivro);
-    res.status(201).json(novoLivro);
 });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,13 +1,37 @@
 const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
 const livrosRouter = require('./livros');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API de Livros',
+            version: '1.0.0',
+            description: 'API para gerenciar livros',
+        },
+        servers: [
+            {
+                url: `http://localhost:${PORT}`,
+            },
+        ],
+    },
+    apis: ['./src/livros.js'], 
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.get('/', (req, res) => {
+    res.send('API de Livros Funcionando!');
+});
+
 app.use('/livros', livrosRouter);
 
 app.listen(PORT, () => {
